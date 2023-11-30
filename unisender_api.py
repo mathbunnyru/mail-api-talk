e69386118b3bbffdef49d20d54a0180792bbe7ac
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-11-30 18:17:01 +0100

    Get result in make_request

--- unisender_api.py
+++ unisender_api.py
@@ -8,14 +8,13 @@ BASE_URL = "https://api.unisender.com/{LANG}/api/{METHOD}"
 __API_KEY = (Path.home() / ".talk" / "unisender_api_key").read_text()
 
 
-def make_request(
-    method: str, params: dict[str, Any], lang: str = DEFAULT_LANG
-) -> dict[str, Any]:
+def make_request(method: str, params: dict[str, Any], lang: str = DEFAULT_LANG) -> Any:
     resp = requests.post(
         BASE_URL.format(LANG=lang, METHOD=method),
         params=params,
         data={"api_key": __API_KEY},
-        timeout=1,
+        timeout=10,
     )
     resp.raise_for_status()
-    return resp.json()  # type: ignore
+    print(resp.json())
+    return resp.json()["result"]  # type: ignore
