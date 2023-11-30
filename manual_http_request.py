b88816dd0682b62dbc5999af3c6f1a4766ffea15
Ayaz Salikhov <mathbunnyru@gmail.com>
2023-11-30 15:42:33 +0100

    Add LINE_BREAK

--- manual_http_request.py
+++ manual_http_request.py
@@ -2,11 +2,13 @@
 # create connections
 import socket
 
+LINE_BREAK = b"\r\n"
+
 # address of httpbin.org
 address = "75.101.131.185"
 port = 80
 
-request = b"\r\n".join((b"GET /get HTTP/1.1", b"Host: httpbin.org")) + b"\r\n\r\n"
+request = LINE_BREAK.join((b"GET /get HTTP/1.1", b"Host: httpbin.org")) + 2 * LINE_BREAK
 
 with socket.socket(
     family=socket.AF_INET, type=socket.SOCK_STREAM  # Address Family INET  # TCP
